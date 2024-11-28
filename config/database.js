dcc29c5a9f2f38d306381964c1b5f1d07828dabe
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-11-28 18:28:52 -0500

    Database: Sequelize db

--- config/database.js
+++ config/database.js
@@ -1,16 +1,48 @@
-const {Client} = require('pg');
-require('dotenv').config();
+const { Sequelize, Op } = require('sequelize');
 
-const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432
 });
 
-client.connect()
-    .then(() => console.log('Connected to database'))
-    .catch((error) => console.error('Error connecting to database', error.stack));
+if(process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        port: process.env.DB_PORT || 5432,
+        host: process.env.DB_HOST || 'localhost',
+    });
+}
 
-module.exports = client;
+
+module.exports = { sequelize, Op };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const {Client} = require('pg');
+// require('dotenv').config();
+
+// const client = new Client({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASS,
+//     port: process.env.DB_PORT,
+// });
+
+// client.connect()
+//     .then(() => console.log('Connected to database'))
+//     .catch((error) => console.error('Error connecting to database', error.stack));
+
+// module.exports = client;
--- server.js
+++ server.js
@@ -2,10 +2,22 @@
 require('dotenv').config();
 const app = require('./app');
 
+const { sequelize } = require('./config/database');
+
 //PORT
 const PORT = process.env.PORT || 4444;
 
-//Listen
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Sync Sequelize and Start Server
+(async () => {
+    try {
+      await sequelize.authenticate();
+      console.log('Database connected successfully.');
+  
+      app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+      });
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+      process.exit(1); 
+    }
+  })();
