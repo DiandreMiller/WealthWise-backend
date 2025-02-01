6ac3e940a10854fe4488830efc0f59110b36db43
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-01-31 22:45:03 -0500

    Functionality: Decrypted user id for update budget function

--- app.js
+++ app.js
@@ -36,64 +36,64 @@ const Budget = require('./models/budgetModel');
 
 
 // Block's API Testing Tools
-app.use((request, response, next) => {
-    const userAgent = request.headers['user-agent'];
+// app.use((request, response, next) => {
+//     const userAgent = request.headers['user-agent'];
     
-    const blockedAgents = [
-        'Postman', 'Insomnia', 'Paw', 'Swagger', 'curl', 'HTTPie', 
-        'Apigee', 'Rest-Assured', 'JMeter', 'Karate DSL', 'Tavern', 'Hoppscotch', 
-        'Newman', 'Assertible', 'TestMace', 'Beeceptor', 'API Fortress', 'Runscope', 
-        'Wget', 'Arthas Tunnel', 'Bat', 'Hurl', 'Restish', 'Axios Mock Adapter', 
-        'fetch-mock', 'Mocha', 'Cypress', 'Nightwatch.js', 'PyRestTest', 'Locust', 
-        'pytest-httpx', 'HTTPretty', 'WireMock', 'Spock Framework', 'TestNG', 
-        'VCR', 'Faraday', 'RSpec API Documentation', 'Guzzle', 'Laravel Dusk', 
-        'Artillery', 'Gatling', 'Vegeta', 'Boom', 'PostgREST', 'JSON Server', 
-        'MSW (Mock Service Worker)', 'Prism', 'OWASP ZAP', 'Burp Suite', 
-        'Nikto', 'Mitmproxy', 'SmartBear ReadyAPI', 'BlazeMeter', 'Testim', 
-        'Thunder Client', 'REST Client', 'Postwoman (Hoppscotch)', 'Tyk Test Framework', 
-        'FastAPI TestClient', 'Grpcurl', 'RoboHydra', 'Apache HttpClient', 
-        'Zerocode', 'Step CI', 'Spectral', 'Fortio', 'Apiritif', 'SoapUI', 
-        'GraphiQL', 'Altair', 'Mockoon', 'Swagger UI', 'Chakram', 'Supertest', 
-        'K6', 'WireMock', 'API Tester', 'Boomi', 'LoopBack Explorer', 'API Blueprint', 
-        'CloudTest', 'SlashDB', 'LambdaTest', 'Dredd', 'API Gateway Testing Console', 
-        'RestCase', 'Nock', 'MockServer', 'Mirage JS', 'Traffic Parrot', 'Hoverfly', 
-        'MockLab', 'Interceptor', 'Scriptrunner', 'Kong', 'LocalStack', 'Acunetix', 
-        'Nessus', 'NetSparker', 'Qualys', 'Burp API Scanner', 'SecApps', 'SSLyze', 
-        'Intruder.io', 'API Security.io', 'Firebug Lite', 'API Buddy', 'Fiddler Everywhere', 
-        'Boomerang', 'Advanced REST Client', 'ReqBin', 'ReqRes', 'HTTP Debugger', 
-        'Charles Proxy', 'BrowserStack API Testing', 'Nightfall AI', 'HTTP Toolkit', 
-        'Smocker', 'HTTP Prompt', 'FakeIt', 'JQ', 'HTTPRequester', 'Netcat', 
-        'TLSScan', 'SockJS', 'NanoHTTPD', 'Feign', 'OkHttp', 'Unirest', 'Retrofit', 
-        'Spring WebTestClient', 'Requests-Mock', 'WebTest', 'Testify', 
-        'Falcon Testing Library', 'Tornado HTTPClient', 'FrisbyJS', 'PactJS', 
-        'Puppeteer', 'Jasmine-ajax', 'Jest Mock Fetch', 'Zend HTTP Client', 
-        'PHPUnit REST API Testing', 'PHPSpec', 'Cucumber', 'Typhoeus', 'Resty', 
-        'Golang HTTP Mock', 'Loader.io', 'Tsung', 'NeoLoad', 'LoadNinja', 'WebLOAD', 
-        'OpenSTA', 'Locust', 'Siege', 'Hey', 'Taurus', 'Postman Monitors', 
-        'Uptrends', 'StatusCake', 'Pingdom', 'Datadog API Monitoring', 'Uptime Robot', 
-        'Moesif', 'Dotcom-Monitor', 'Splunk Synthetic Monitoring', 'Sentry', 
-        'Parasoft SOAtest', 'Telerik Test Studio', 'Protractor', 'Katalon Studio', 
-        'TestProject', 'API-Checker', 'ScriptRunner for APIs', 'Mocky.io', 
-        'Faker.js', 'Pyngrok', 'Clouditor', 'Firecamp', 'FoxyProxy', 'Grizzly HTTP', 
-        'HTTPBin', 'Requestly', 'WireMock Cloud', 'SwaggerHub', 'Gravitee', 
-        'WcfStorm', 'API Umbrella', 'Redocly', 'API Science', 'Dart HTTP Client', 
-        'Hitch', 'OpenAPI Diff', 'Swagger Validator', 'RoboHydra Server', 'OverOps', 
-        'StormForge', 'LoadView', 'Tyk Dashboard', 'Kubernetes Kube-API', 
-        'AquaSec Kube-Bench', 'AppDynamics API Monitoring', 'Keycloak REST Client', 
-        'Dredd CLI', 'Tyk Gateway', 'Grizzly Load Tester', 'Restlet Client', 
-        'Fiddler Classic', 'K6 Cloud', 'Parrot', 'Swagger Codegen', 'APIsec', 
-        'Zanshin API Scanner', 'Imperva API Security', 'Salt Security', 
-        'Traceable AI', 'Sauce Labs API Testing', 'SmartMock.io', 'Snapshooter', 
-        'Reflect', 'Vialer.js', 'NGINX Amplify', 'GraphQL Inspector'
-    ];
+//     const blockedAgents = [
+//         'Postman', 'Insomnia', 'Paw', 'Swagger', 'curl', 'HTTPie', 
+//         'Apigee', 'Rest-Assured', 'JMeter', 'Karate DSL', 'Tavern', 'Hoppscotch', 
+//         'Newman', 'Assertible', 'TestMace', 'Beeceptor', 'API Fortress', 'Runscope', 
+//         'Wget', 'Arthas Tunnel', 'Bat', 'Hurl', 'Restish', 'Axios Mock Adapter', 
+//         'fetch-mock', 'Mocha', 'Cypress', 'Nightwatch.js', 'PyRestTest', 'Locust', 
+//         'pytest-httpx', 'HTTPretty', 'WireMock', 'Spock Framework', 'TestNG', 
+//         'VCR', 'Faraday', 'RSpec API Documentation', 'Guzzle', 'Laravel Dusk', 
+//         'Artillery', 'Gatling', 'Vegeta', 'Boom', 'PostgREST', 'JSON Server', 
+//         'MSW (Mock Service Worker)', 'Prism', 'OWASP ZAP', 'Burp Suite', 
+//         'Nikto', 'Mitmproxy', 'SmartBear ReadyAPI', 'BlazeMeter', 'Testim', 
+//         'Thunder Client', 'REST Client', 'Postwoman (Hoppscotch)', 'Tyk Test Framework', 
+//         'FastAPI TestClient', 'Grpcurl', 'RoboHydra', 'Apache HttpClient', 
+//         'Zerocode', 'Step CI', 'Spectral', 'Fortio', 'Apiritif', 'SoapUI', 
+//         'GraphiQL', 'Altair', 'Mockoon', 'Swagger UI', 'Chakram', 'Supertest', 
+//         'K6', 'WireMock', 'API Tester', 'Boomi', 'LoopBack Explorer', 'API Blueprint', 
+//         'CloudTest', 'SlashDB', 'LambdaTest', 'Dredd', 'API Gateway Testing Console', 
+//         'RestCase', 'Nock', 'MockServer', 'Mirage JS', 'Traffic Parrot', 'Hoverfly', 
+//         'MockLab', 'Interceptor', 'Scriptrunner', 'Kong', 'LocalStack', 'Acunetix', 
+//         'Nessus', 'NetSparker', 'Qualys', 'Burp API Scanner', 'SecApps', 'SSLyze', 
+//         'Intruder.io', 'API Security.io', 'Firebug Lite', 'API Buddy', 'Fiddler Everywhere', 
+//         'Boomerang', 'Advanced REST Client', 'ReqBin', 'ReqRes', 'HTTP Debugger', 
+//         'Charles Proxy', 'BrowserStack API Testing', 'Nightfall AI', 'HTTP Toolkit', 
+//         'Smocker', 'HTTP Prompt', 'FakeIt', 'JQ', 'HTTPRequester', 'Netcat', 
+//         'TLSScan', 'SockJS', 'NanoHTTPD', 'Feign', 'OkHttp', 'Unirest', 'Retrofit', 
+//         'Spring WebTestClient', 'Requests-Mock', 'WebTest', 'Testify', 
+//         'Falcon Testing Library', 'Tornado HTTPClient', 'FrisbyJS', 'PactJS', 
+//         'Puppeteer', 'Jasmine-ajax', 'Jest Mock Fetch', 'Zend HTTP Client', 
+//         'PHPUnit REST API Testing', 'PHPSpec', 'Cucumber', 'Typhoeus', 'Resty', 
+//         'Golang HTTP Mock', 'Loader.io', 'Tsung', 'NeoLoad', 'LoadNinja', 'WebLOAD', 
+//         'OpenSTA', 'Locust', 'Siege', 'Hey', 'Taurus', 'Postman Monitors', 
+//         'Uptrends', 'StatusCake', 'Pingdom', 'Datadog API Monitoring', 'Uptime Robot', 
+//         'Moesif', 'Dotcom-Monitor', 'Splunk Synthetic Monitoring', 'Sentry', 
+//         'Parasoft SOAtest', 'Telerik Test Studio', 'Protractor', 'Katalon Studio', 
+//         'TestProject', 'API-Checker', 'ScriptRunner for APIs', 'Mocky.io', 
+//         'Faker.js', 'Pyngrok', 'Clouditor', 'Firecamp', 'FoxyProxy', 'Grizzly HTTP', 
+//         'HTTPBin', 'Requestly', 'WireMock Cloud', 'SwaggerHub', 'Gravitee', 
+//         'WcfStorm', 'API Umbrella', 'Redocly', 'API Science', 'Dart HTTP Client', 
+//         'Hitch', 'OpenAPI Diff', 'Swagger Validator', 'RoboHydra Server', 'OverOps', 
+//         'StormForge', 'LoadView', 'Tyk Dashboard', 'Kubernetes Kube-API', 
+//         'AquaSec Kube-Bench', 'AppDynamics API Monitoring', 'Keycloak REST Client', 
+//         'Dredd CLI', 'Tyk Gateway', 'Grizzly Load Tester', 'Restlet Client', 
+//         'Fiddler Classic', 'K6 Cloud', 'Parrot', 'Swagger Codegen', 'APIsec', 
+//         'Zanshin API Scanner', 'Imperva API Security', 'Salt Security', 
+//         'Traceable AI', 'Sauce Labs API Testing', 'SmartMock.io', 'Snapshooter', 
+//         'Reflect', 'Vialer.js', 'NGINX Amplify', 'GraphQL Inspector'
+//     ];
     
     
-    if (blockedAgents.some(agent => userAgent && userAgent.includes(agent))) {
-        return response.status(403).send('Requests from API testing tools are not allowed.');
-    }
+//     if (blockedAgents.some(agent => userAgent && userAgent.includes(agent))) {
+//         return response.status(403).send('Requests from API testing tools are not allowed.');
+//     }
     
-    next();
-});
+//     next();
+// });
 
   
 
